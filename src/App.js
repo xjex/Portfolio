6336ae881a898ec465f9b46b6c49e23286f0d3de
Xavier <dev.xjmanaloto@gmail.com>
2022-10-04 21:59:46 +0800

    Prototype

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import ColorSchemesExample from "./Navigation/navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { FiGithub } from "react-icons/fi";
+import { FiFacebook } from "react-icons/fi";
+import { FiLinkedin } from "react-icons/fi";
+import { FiTwitter } from "react-icons/fi";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ColorSchemesExample />
+      <div id="Head">
+        <h1>Hello, I'm Xavier!</h1>
+        <p>Content Creator | Keyboard Enthusiat | Developer</p>
+        <h2>
+          <FiGithub />
+          | <FiFacebook /> | <FiLinkedin /> | <FiTwitter />
+        </h2>
+      </div>
+      <div id="Head">
+        <h1>Lorem ipsum</h1>
+      </div>
     </div>
   );
 }
